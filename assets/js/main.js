b97122d5bd24ecf1ccef8ec2ad93a5dfd241cff5
eric <ericgip97@gmail.com>
2020-04-25 22:55:26 -0700

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+<script src="https://code.jquery.com/jquery-3.5.0.min.js"
+        integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+
+    
+//
+
+$(document).ready(function(){
+    var count = 0;
+    var time = 31;
+    var isSelected = false;
+    var ticker;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+
