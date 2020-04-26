fb4cc094e42bbfc1dfdbd1319f5abd9b36ad3b2d
eric <ericgip97@gmail.com>
2020-04-26 01:01:05 -0700

    added the start, quiz, and end screen + CSS + start button functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 var timeEl = document.querySelector(".time");
+var startButton = document.querySelector("#startButton")
+var titleScreen = document.querySelector("#titleScreen")
+var quizScreen = document.querySelector("#quiz")
+var endScreen = document.querySelector("#endScreen")
 
 var score = 0;
 var secondsLeft = 30;
 
+// Timer 
 function setTime() {
     var timerInverval = setInterval(function() {
         secondsLeft--;
@@ -15,14 +20,27 @@ function setTime() {
     }, 1000);
 }
 
+// start quiz: needs to get the questions by updating title
+// needs to loop over the choices
+// need to somehow create a button for all of the choices/  
+function startQuiz() {
+    // hide the title screen,
+    titleScreen.setAttribute("class", "hide")
+    // then show quiz questions, choices
+    quizScreen.setAttribute("class", "show")
+    //starting timer at the end, instead of the beginning to be fair. 
+    setTime();
+
+}
 
 // to do, send this to send to finaly screen, just display score and stuff
 // fuction sendToEnd() {
 //     timeEl.textContent = " ";
     
-// }
+// To do, put this inside the startQuiz function?
+// setTime();
 
-
-setTime();
 
 console.log(secondsLeft);
+
+startButton.addEventListener("click", startQuiz)
